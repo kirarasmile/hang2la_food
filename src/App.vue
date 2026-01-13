5c0d71e15c5550b3ceb927af59a3b988927dc357
qzn <2839224397@qq.com>
2026-01-13 14:25:56 +0800

    fix: 添加 Naive UI 全局 Provider 解决 useMessage 报错

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
-import { darkTheme, NConfigProvider, NGlobalStyle } from 'naive-ui'
+import { darkTheme, NConfigProvider, NGlobalStyle, NMessageProvider, NDialogProvider, NNotificationProvider, NLoadingBarProvider } from 'naive-ui'
 import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <NConfigProvider :theme="darkTheme">
     <NGlobalStyle />
-    <RouterView />
+    <NLoadingBarProvider>
+      <NNotificationProvider>
+        <NMessageProvider>
+          <NDialogProvider>
+            <RouterView />
+          </NDialogProvider>
+        </NMessageProvider>
+      </NNotificationProvider>
+    </NLoadingBarProvider>
   </NConfigProvider>
 </template>
 
